2c9363d9282dd18c78c19bd79f6551e7b8084926
DavidTL95 <david.torrado95@gmail.com>
2023-06-21 13:57:08 +0200

    Creacion de la página mainApp y el array con los electrodomesticos

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-let elec1 = new Electrodomestico('rojo', 'A', 50);
-console.log(elec1);
+// let elec1 = new Electrodomestico('rojo', 'A', 50);
+// console.log(elec1);
 
 //     console.log(`Color: ${elec1.getColor()}`);
 //     console.log(`Consumo: ${elec1.getConsumo()}`);
 //     console.log(`Peso: ${elec1.getPeso()}`);
 //     console.log(`Precio final: ${elec1.getPrecio()}`);
 
-let lav1 = new Lavadora('blanco', 'F', 50);
-console.log(lav1);
+// let lav1 = new Lavadora('blanco', 'F', 50);
+// console.log(lav1);
 
-let lav2 = new Lavadora('blanco', 'F', 50, 50);
-console.log(lav2);
+// let lav2 = new Lavadora('blanco', 'F', 50, 50);
+// console.log(lav2);
 
-let tele1 = new Television('blanco', 'A', 20, 50, false)
-console.log(tele1);
-let tele2 = new Television('blanco', 'A', 20, 50, true)
-console.log(tele2);
+// let tele1 = new Television('blanco', 'A', 20, 50, false)
+// console.log(tele1);
+// let tele2 = new Television('blanco', 'A', 20, 50, true)
+// console.log(tele2);
---
+++ src/mainApp.ts
@@ -0,0 +1,12 @@
+
+let television1 = new Television('blanco', 'A', 20, 40, false);
+let television2 = new Television('negro', 'D', 50, 50, true);
+let lav2 = new Lavadora('blanco', 'F', 50, 50);
+let lav1 = new Lavadora('blanco', 'F', 50);
+let elec1 = new Electrodomestico('rojo', 'A', 50);
+let lav3 = new Lavadora('gris', 'B', 30);
+
+
+let arrayElectrodomesticos : Electrodomestico[] = [television1, television2, lav1, lav2, elec1, lav3];
+
+console.log(arrayElectrodomesticos);
